8392a93904d53b275fbb42998a63838f15253e8a
AtomicSage22 <dev.dev.singh.chauhan@gmail.com>
2023-03-30 11:33:08 +0530

    add function to update DOM

--- src/index.js
+++ src/index.js
@@ -1,7 +1,54 @@
 import { fetchWeather } from "./weatherData";
 
-async function meow(){
-    console.log(await fetchWeather("bhopal"));
+const temperature = document.querySelector(".temp")
+const locationCity = document.querySelector(".city");
+const locationCountry = document.querySelector(".country");
+const precipitation = document.querySelector(".precipitation");
+const humidity = document.querySelector(".humidity");
+const wind = document.querySelector(".wind");
+const day = document.querySelector(".day");
+const date = document.querySelector(".date");
+const weatherIcon = document.querySelector(".icon");
+const background = document.querySelector("video");
+
+let unit = true;
+
+async function updateWeather(city, unit){
+    const data = (await fetchWeather(city, unit));
+    console.log(data.date);
+    day.textContent = `${data.day},`;
+    date.textContent = data.date;
+    temperature.textContent = data.temp_c;  
+    locationCity.textContent = data.city;
+    locationCountry.textContent = data.country;
+    precipitation.textContent = `Precipitation: ${data.precipitation}`;
+    humidity.textContent = `Humidity: ${data.humidity}`;
+    wind.textContent = `Wind: ${data.wind}`;
+    if (data.code < 1003){
+        weatherIcon.setAttribute("src", "assets/sunny.png");
+        background.setAttribute("src", "assets/sunny.mp4");
+    }
+    if(data.code >= 1003 && data.code < 1030){
+        weatherIcon.setAttribute("src", "assets/cloudy.png");
+        background.setAttribute("src", "assets/cloudy.mp4");
+    }
+    if((data.code >= 1063 && data.code < 1066) || (data.code == 1072) ||(data.code >= 1150 && data.code <= 1201) || (data.code >= 1240 && data.code <= 1246)){
+        weatherIcon.setAttribute("src", "assets/rainy.png")
+        background.setAttribute("src", "assets/rainy.mp4");
+    }
+    if((data.code >=1066 && data.code <= 1069) || data.code == 1114 || data.code == 1117 || (data.code >= 1204 && data.code <= 1237) || (data.code >= 1249 && data.code <= 1264)){
+        weatherIcon.setAttribute("src", "assets/snow.png");
+        background.setAttribute("src", "assets/snowy.mp4");
+    }
+    if((data.code == 1087) || (data.code >= 1273 && data.code <= 1282)){
+        weatherIcon.setAttribute("src", "assets/thunder.png");
+        background.setAttribute("src", "assets/thunder.mp4");
+    }
+    if(data.code == 1030 || data.code == 1135 || data.code == 1147){
+        weatherIcon.setAttribute("src", "assets/foggy.png");
+        background.setAttribute("src", "assets/foggy.mp4");
+    }
+
 }
-meow();
+updateWeather("bhopal", unit);
 
--- src/weatherData.js
+++ src/weatherData.js
@@ -2,16 +2,35 @@ export const fetchWeather = async (city, unit) =>{
     const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=3cbf4f7a5dac4b9cacb43804232403&q=${city}`)
     const data = await response.json()
     console.log(data);
-    if (unit == 1){
+    const weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    const months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    const date = new Date(data.location.localtime);
+    if (unit == true){
         return {
+            code: data.current.condition.code,
+            day: weekDays[date.getDay()],
+            date: `${date.getDate()}  ${months[date.getMonth()]}`,
+            precipitation: `${data.current.precip_mm}mm`,
+            humidity: data.current.humidity,
+            wind: `${data.current.wind_kph}kph`,
+            city: data.location.name,
+            country: data.location.country,
             condition: data.current.condition.text,
-            temp_c: data.current.temp_c
+            temp_c: `${data.current.temp_c}°C`
         };
     }
     else{
         return {
+            code: data.current.condition.code,
+            day: weekDays[date.getDay()],
+            date: date.getDate(),
+            precipitation: `${data.current.precip_in}in`,
+            humidity: data.current.humidity,
+            wind: `${data.current.wind_mph}mph`,
+            city: data.location.name,
+            country: data.location.country,
             condition: data.current.condition.text,
-            temp_f: data.current.temp_f
+            temp_f: `${data.current.temp_f}°F`
         }
     }
 }
